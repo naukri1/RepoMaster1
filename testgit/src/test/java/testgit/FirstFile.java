f2240deff704aa4e95673ed052a8714b6e742b28
Neo <gituser1985@gmail.com>
2023-03-12 11:53:58 +0530

    first commit

---
+++ testgit/src/test/java/testgit/FirstFile.java
@@ -0,0 +1,19 @@
+package testgit;
+
+import org.testng.annotations.Test;
+
+public class FirstFile {
+
+	
+	@Test
+	public void methodA() {
+		
+		System.out.println("This is method A from firstfile");
+	}
+	
+	@Test
+	public void methodB() {
+		
+		System.out.println("This is method B from firstfile");
+	}
+}
---
+++ testgit/src/test/java/testgit/SecondFile.java
@@ -0,0 +1,19 @@
+package testgit;
+
+import org.testng.annotations.Test;
+
+public class SecondFile {
+
+	
+	@Test
+	public void methodC() {
+		
+		System.out.println("This is method C from Secondfile");
+	}
+	
+	@Test
+	public void methodD() {
+		
+		System.out.println("This is method D from Secondfile");
+	}
+}
